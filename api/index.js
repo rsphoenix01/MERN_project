f6a19c7a237640e052b829e6f9387d8e9e8bc8f7
roshan <roshan@gmail.com>
2024-02-13 14:36:59 +0530

    ok

---
+++ api/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDB Connected');
+    }).catch(err => {
+        console.log(err);
+    });
+
+const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000!!");
+}); 
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,125 @@
+import { Avatar, Button, Dropdown, DropdownItem, Navbar, TextInput} from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon, FaSun} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
+import { signOutSuccess } from '../redux/user/userSlice'
+
+export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const {currentUser} = useSelector(state => state.user);
+    const {theme} = useSelector(state => state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
+
+    const handleSignOut = async() => {
+        try {
+          const res = await fetch('/api/user/signout', {
+            method: 'POST',
+          });
+          if(!res.ok){
+            console.log(data.message);
+          } else {
+            dispatch(signOutSuccess());
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    }
+  return (
+    <Navbar className='border-b-2'>
+        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+            The
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Thinking Pen</span>
+        </Link>
+        <form onSubmit={handleSubmit}>
+            <TextInput 
+                type='text'
+                placeholder='search'
+                rightIcon={AiOutlineSearch}
+                className='hidden lg:inline'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            ></ TextInput>
+        </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <AiOutlineSearch></AiOutlineSearch>
+        </Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+                {theme === 'light' ? <FaMoon></FaMoon> : <FaSun></FaSun>}
+            </Button>
+            {currentUser ? (
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                        <Avatar
+                            alt='user'
+                            img={currentUser.profilePicture}
+                            rounded
+                        >
+                        </Avatar>
+                    }
+                >
+                    <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to='/dashboard?tab=profile'>
+                        <DropdownItem>Profile</DropdownItem>
+                    </Link>
+                    <Dropdown.Divider></Dropdown.Divider>
+                    <DropdownItem onClick={handleSignOut}>Sign Out</DropdownItem>
+                </Dropdown>
+            ) : (
+                <>
+                <Link to="/sign-in">
+                <Button gradientDuoTone='purpleToBlue' outline>
+                    Sign In
+                </Button>
+            </Link>
+                </>
+            )}
+            <Navbar.Toggle></Navbar.Toggle>
+        </div>
+        <Navbar.Collapse>
+            <Navbar.Link active={path==="/"} as={'div'}>
+                <Link to='/'>
+                    Home
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path==="/about"} as={'div'}>
+                <Link to='/about'>
+                    About
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path==="/projects"} as={'div'}>
+                <Link to='/projects'>
+                    Projects
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
+    </Navbar>
+  )
+}
