c2642b88022e1205ddfdcb5821969ab93a3590f8
rsphoenix01 <73596661+rsphoenix01@users.noreply.github.com>
2024-02-13 15:53:19 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -114,11 +114,7 @@ export default function Header() {
                     About
                 </Link>
             </Navbar.Link>
-            <Navbar.Link active={path==="/projects"} as={'div'}>
-                <Link to='/projects'>
-                    Projects
-                </Link>
-            </Navbar.Link>
+            
         </Navbar.Collapse>
     </Navbar>
   )
